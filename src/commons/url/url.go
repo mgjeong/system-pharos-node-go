a3b48e8a6049443b036503755cae23a8575b6877
hyuna0213.jo <hyuna0213.jo@samsung.com>
2017-12-01 10:14:49 +0900

    Add commons package
    errors, logger, url packages are added.
    
    Signed-off-by: hyuna0213.jo <hyuna0213.jo@samsung.com>

---
+++ src/commons/url/url.go
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright 2017 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *******************************************************************************/
+
+// Package commons/url defines url used by system-management-agent.
+package url
+
+// Returning base url as string.
+func Base() string { return "/api/v1" }
+
+// Returning deploy url as string.
+func Deploy() string { return "/deploy" }
+
+// Returning Apps url as string.
+func Apps() string { return "/apps" }
+
+// Returning Update url as string.
+func Update() string { return "/update" }
+
+// Returning Start url as string.
+func Start() string { return "/start" }
+
+// Returning Stop url as string.
+func Stop() string { return "/stop" }
+
+// Returning Agents url as string.
+func Agents() string { return "/agents" }
+
+// Returning Ping url as string.
+func Ping() string { return "/ping" }
+
+// Returning Register url as string.
+func Register() string { return "/register" }
+
+// Returning Unregister url as string.
+func Unregister() string { return "/unregister" }
