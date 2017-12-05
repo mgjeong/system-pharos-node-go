e3daa2cdd639a0ef205bb23cb7a59ab786d116d8
Parkhi <h_w.park@samsung.com>
2017-12-05 18:31:38 +0900

    Add db model.
        - added db model related with deployment feature.
    
    Signed-off-by: Parkhi <h_w.park@samsung.com>

---
+++ src/db/modelinterface/service.go
@@ -0,0 +1,28 @@
+package modelinterface
+
+import (
+)
+
+// Interface of Service model's operations.
+type Service interface {
+	// InsertComposeFile insert docker-compose file for new service.
+	InsertComposeFile(description string) (map[string]interface{}, error)
+
+	// GetAppList returns all of app's IDs.
+	GetAppList() ([]map[string]interface{}, error)
+
+	// GetApp returns docker-compose data of target app.
+	GetApp(app_id string) (map[string]interface{}, error)
+
+	// UpdateAppInfo updates docker-compose data of target app.
+	UpdateAppInfo(app_id string, description string) error
+
+	// DeleteApp delete docker-compose data of target app.
+	DeleteApp(app_id string) error
+
+	// GetAppState returns app's state
+	GetAppState(app_id string) (string, error)
+
+	// UpdateAppState updates app's State.
+	UpdateAppState(app_id string, state string) error
+}
